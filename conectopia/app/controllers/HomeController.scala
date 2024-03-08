d979b9c2fbe161fce5be20c6bac82bdb565bd0c2
Sergiogd112 <34186354+Sergiogd112@users.noreply.github.com>
2024-03-08 15:40:19 +0100

    adding

--- conectopia/app/controllers/HomeController.scala
+++ conectopia/app/controllers/HomeController.scala
@@ -21,4 +21,8 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
+  def about() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.about())
+  }
+
 }
