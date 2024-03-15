d6239fcf58e701d60371937527165b69a7044091
bryan834 <bryan.garcia@estudiantat.upc.edu>
2024-03-15 18:15:32 +0100

    update

--- play-1.5.3/Conectopia/app/controllers/Application.java
+++ play-1.5.3/Conectopia/app/controllers/Application.java
@@ -12,20 +12,37 @@ public class Application extends Controller {
     public static void index() {
         render();
     }
-    public static void login()
-    {
+
+    public static void login() {
+
         render();
     }
-    public static void loginPost()
-    {
+
+    public static void loginPost(String emailuname, String password) {
+        // check if emailUname is email or username if it contains @ then it is email
+        if (emailuname.contains("@")) {
+            User u = User.find("byEmailAndPassword", emailuname, password).first();
+            if (u == null) {
+                renderText("Usuario no encontrado");
+            } else {
+                renderText("Usuario encontrado");
+            }
+        } else {
+            User u = User.find("byNameAndPassword", emailuname, password).first();
+            if (u == null) {
+                renderText("Usuario no encontrado");
+            } else {
+                renderText("Usuario encontrado");
+            }
+        }
+    }
+
+    public static void register() {
+
         render();
     }
-    public static void register()
-    {
-        render();
-    }
-    public static void registerPost()
-    {
+
+    public static void registerPost() {
         User u = User.find("byName").first();
         render();
     }
