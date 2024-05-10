791c9487c5bbbbf7d11465d7890e482e6aee0fd4
Sergiogd112 <34186354+Sergiogd112@users.noreply.github.com>
2024-05-10 16:49:44 +0200

    conexion server

--- Android/Conectopia/app/src/main/java/com/conectopia/conectopia/ui/home/HomeFragment.java
+++ Android/Conectopia/app/src/main/java/com/conectopia/conectopia/ui/home/HomeFragment.java
@@ -1,6 +1,8 @@
 package com.conectopia.conectopia.ui.home;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +14,12 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.conectopia.conectopia.R;
 import com.conectopia.conectopia.databinding.FragmentHomeBinding;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class HomeFragment extends Fragment {
     int count = 0;
@@ -41,6 +49,7 @@ public class HomeFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
     public void plusOne() {
         View root = binding.getRoot();
 
@@ -48,5 +57,60 @@ public class HomeFragment extends Fragment {
         // write the count to the ui MytextView
         this.homeViewModel.mText.setValue(String.valueOf(this.count));
 
+
     }
+
+    public void connectServerGET(View view) {
+
+        new Thread(new Runnable() {
+            InputStream stream = null;
+            String str = "";
+            String result = null;
+            Handler handler = new Handler();
+
+            public void run() {
+
+                try {
+                    String query = String.format("http://10.0.0.2:9000/api/servers");
+                    URL url = new URL(query);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setReadTimeout(10000);
+                    conn.setConnectTimeout(15000 /* milliseconds */);
+                    conn.setRequestMethod("GET");
+                    conn.setDoInput(true);
+                    conn.connect();
+                    stream = conn.getInputStream();
+
+                    BufferedReader reader = null;
+
+                    StringBuilder sb = new StringBuilder();
+
+                    reader = new BufferedReader(new InputStreamReader(stream));
+
+                    String line = null;
+                    while ((line = reader.readLine()) != null) {
+                        sb.append(line);
+                    }
+                    result = sb.toString();
+                    conn.disconnect();
+                    // Mostrar resultat en el quadre de text.
+                    // Codi incorrecte
+                    // EditText n = (EditText) findViewById (R.id.edit_message);
+                    //n.setText(result);
+
+                    //Codi correcte
+                    Log.i("serverTest", result);
+                    handler.post(new Runnable() {
+                        public void run() {
+                            homeViewModel.mText.setValue(String.valueOf(result));
+                        }
+                    });
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
 }
