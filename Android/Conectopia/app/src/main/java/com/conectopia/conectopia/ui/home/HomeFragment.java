f8de755d86719d14cf6e2a65164e6de4fe63641b
Sergiogd112 <34186354+Sergiogd112@users.noreply.github.com>
2024-05-10 15:33:56 +0200

    update android

--- Android/Conectopia/app/src/main/java/com/conectopia/conectopia/ui/home/HomeFragment.java
+++ Android/Conectopia/app/src/main/java/com/conectopia/conectopia/ui/home/HomeFragment.java
@@ -10,22 +10,29 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.conectopia.conectopia.R;
 import com.conectopia.conectopia.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
+    int count = 0;
 
     private FragmentHomeBinding binding;
+    private HomeViewModel homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
+        this.homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        this.homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        root.findViewById(
+                R.id.button
+        ).setOnClickListener(v -> plusOne());
+
         return root;
     }
 
@@ -33,5 +40,13 @@ public class HomeFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+    public void plusOne() {
+        View root = binding.getRoot();
+
+        this.count++;
+        // write the count to the ui MytextView
+        this.homeViewModel.mText.setValue(String.valueOf(this.count));
+
     }
 }
