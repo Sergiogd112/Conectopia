1285221b4b17898a2c795d855f2c9f825125b672
Bryan834 <bryan.garcia@estudiantat.upc.edu>
2024-05-19 17:47:52 +0200

    Conexion sesion LoginDataSource

--- Android/Conectopia/app/src/main/java/com/conectopia/conectopia/data/LoginDataSource.java
+++ Android/Conectopia/app/src/main/java/com/conectopia/conectopia/data/LoginDataSource.java
@@ -1,9 +1,16 @@
 package com.conectopia.conectopia.data;
 
-import com.conectopia.conectopia.data.model.LoggedInUser;
+import android.os.Handler;
 import android.util.Log;
 
+import com.conectopia.conectopia.data.model.LoggedInUser;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Class that handles authentication w/ login credentials and retrieves user information.
@@ -11,9 +18,41 @@ import java.io.IOException;
 public class LoginDataSource {
 
     public Result<LoggedInUser> login(String username, String password) {
-        Log.i("LoginDataSource", "login: " + username + " " + password);
+        InputStream stream = null;
+        String str = "";
+        String result = null;
+        Handler handler = new Handler();
 
         try {
+            String query = String.format("http://10.0.2.2:9000/api/servers");
+            URL url = new URL(query);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            conn.connect();
+            stream = conn.getInputStream();
+
+            BufferedReader reader = null;
+
+            StringBuilder sb = new StringBuilder();
+
+            reader = new BufferedReader(new InputStreamReader(stream));
+
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+            result = sb.toString();
+            conn.disconnect();
+            // Mostrar resultat en el quadre de text.
+            // Codi incorrecte
+            // EditText n = (EditText) findViewById (R.id.edit_message);
+            //n.setText(result);
+
+            //Codi correcte
+            Log.i("serverTest", result);
             // TODO: handle loggedInUser authentication
             LoggedInUser fakeUser =
                     new LoggedInUser(
