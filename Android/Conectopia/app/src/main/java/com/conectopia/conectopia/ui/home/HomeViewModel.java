8bb175c5ff874d6376c85d63275ef223b848cac4
Bryan834 <bryan.garcia@estudiantat.upc.edu>
2024-06-08 21:25:45 +0200

    HomeFragment, HomeViewModel, fragment_home.xml adaptados con el handler

--- Android/Conectopia/app/src/main/java/com/conectopia/conectopia/ui/home/HomeViewModel.java
+++ Android/Conectopia/app/src/main/java/com/conectopia/conectopia/ui/home/HomeViewModel.java
@@ -7,14 +7,25 @@ import androidx.lifecycle.ViewModel;
 public class HomeViewModel extends ViewModel {
 
     public final MutableLiveData<String> mText;
+    public final MutableLiveData<String> mServerName;
+    public final MutableLiveData<String> mDescription;
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is home fragment");
-
+        mServerName = new MutableLiveData<>();
+        mDescription = new MutableLiveData<>();
     }
 
     public LiveData<String> getText() {
         return mText;
+    }
+
+    public LiveData<String> getServerName() {
+        return mServerName;
+    }
+
+    public LiveData<String> getDescription() {
+        return mDescription;
     }
 }
